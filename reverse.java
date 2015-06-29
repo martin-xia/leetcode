8d9e05beb81b9618c12aa7526b7ee136ddb46a2b
martin.xia <mxia@martinxia.me>
2015-06-29 10:02:14 -0700

    Small fix on overflow

--- reverse.java
+++ reverse.java
@@ -15,8 +15,8 @@
             int temp=(int)(x/10);
             sum=sum*10+(x-10*temp);
             x=temp;
-            if(sum>=2147483647) return 0;
-            if(sum<=-2147483648) return 0;
+            if(sum>2147483647) return 0;
+            if(sum<-2147483648) return 0;
         }
         return isPositive*(int)sum;
     }
