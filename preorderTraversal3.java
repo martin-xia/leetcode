f3125d658ece90a3aace6eee874ba5fcf0b266ef
martin.xia <mxia@martinxia.me>
2015-09-29 16:31:35 -0700

    Leetcode 144: Binary Tree Preorder Traversal

---
+++ preorderTraversal3.java
@@ -0,0 +1,45 @@
+/*
+ * Leetcode 144: Binary Tree Preorder Traversal
+ * Morris Algorithm
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * Martin.Xia
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while(cur != null)
+        {
+            if(cur.left == null) {
+                res.add(cur.val);
+                cur = cur.right;
+            }
+            else {
+                pre = cur.left;
+                while(pre.right!=null && pre.right!=cur)
+                    pre = pre.right;
+                if(pre.right==null) {
+                    res.add(cur.val);
+                    pre.right = cur;
+                    cur = cur.left;
+                }
+                else {
+                    pre.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return res;
+    }
+}
